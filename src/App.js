283a74508a8f3529ce4a492dbc15c60fd7d3e93b
Kobi Kadosh <kobi.kadosh@gmail.com>
2018-02-24 19:54:53 +0200

    add graphcool server + initial design for editor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ import Login from './Auth/Login';
 import BillAgendaEditor from './BillAgendaEditor/BillAgendaEditor';
 import BillAgendaViewer from './BillAgendaViewer/BillAgendaViewer';
 
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cje1hr7h70t990128cvmbvsop' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -32,41 +44,27 @@ class App extends Component {
     this.setState({
       isEditor: profile['https://api.bill-agenda.co.il/rule'].includes('editor')
     });
-     // this.auth0Management.login();
    })
    .catch(err => {
      console.error(err);
    });
-
-   // this.auth0Management.handleAuthentication().then(() => {
-   //   this.auth
-   //    .getProfile().then(profile => {
-   //      if (profile) {
-   //        const userId = profile.sub.split('|')[1];
-   //        this.auth0ManageUser = new Auth0Manage(this.auth0Management.getIdToken());
-   //        return this.auth0ManageUser.getUser(userId).then(userInfo => {
-   //          this.setState({ userInfo });
-   //
-   //          return Promise.resolve(userInfo);
-   //        });
-   //      }
-   //    })
-   // });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
 
-        <Login auth={this.auth} />
+          <Login auth={this.auth} />
 
-        {this.state.isEditor ?
-          <BillAgendaEditor /> : <BillAgendaViewer />}
-      </div>
+          {this.state.isEditor ?
+            <BillAgendaEditor /> : <BillAgendaViewer />}
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- src/BillAgendaEditor/BillAgendaEditor.js
+++ src/BillAgendaEditor/BillAgendaEditor.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { compose } from 'recompose';
+import TextField from 'material-ui/TextField';
+
+import AgendasPicker from '../AgendasPicker/AgendasPicker';
 
 const agendasDataSourceUrl = 'http://data.obudget.org/api/queries/1456/results.json?api_key=04011ecd82e3a0127df96ce56ace91b7a71dc764';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  menu: {
+    width: 200,
+  },
+});
 
 class BillAgendaEditor extends Component {
   constructor(props) {
@@ -27,13 +50,71 @@ class BillAgendaEditor extends Component {
     }
   }
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  submit = ({
+    billNumber,
+    corrlelation,
+    description,
+    agendaId,
+    creatorId
+  }) => {
+    const {  mutate } = this.props;
+    mutate({
+      variables: {
+        billNumber,
+        corrlelation,
+        description,
+        agendaId,
+        creatorId,
+      }
+    })
+      .then(({ data }) => {
+        console.log('got data', data);
+      }).catch((error) => {
+        console.log('there was an error sending the query', error);
+      });
+  }
+
+  addAgendas = () => {
+    const {  mutate } = this.props;
+
+    if (!this.state.agendas) {
+      return;
+    }
+
+    let addAgendasPromises = this.state.agendas.map(agenda => {
+      const {
+        id,
+        name
+      } = agenda;
+      return mutate({
+        variables: {
+          id,
+          name
+        }
+      });
+    });
+
+    Promise.all(addAgendasPromises).then(agendasIds => {
+      console.log(agendasIds);
+    })
+  }
+
   render() {
+    const { classes, mutate } = this.props;
+
     return (<div>
-      {this.state.agendas && this.state.agendas.map(agenda => {
-        return <div>
-          {agenda.name}
-        </div>
-      })}
+            {this.state.agendas ? <button onClick={this.addAgendas} >import agendas</button> : null}
+
+      {this.state.agendas ?
+        <AgendasPicker agendas={
+          this.state.agendas.map(agenda => ({ label: agenda.name, ...agenda }))}/>
+          : 'Loading agendas'}
 
       <select>
         <option value="-1.0">התנגדות מלאה</option>
@@ -42,8 +123,56 @@ class BillAgendaEditor extends Component {
         <option value="0.5">תמיכה חלקית</option>
         <option value="1.0">תמיכה מלאה</option>
       </select>
+
+      <TextField
+          id="multiline-flexible"
+          label="Multiline"
+          multiline
+          rowsMax="4"
+          value={this.state.multiline}
+          onChange={this.handleChange('multiline')}
+          className={classes.textField}
+          margin="normal"
+        />
     </div>)
   }
 }
 
-export default BillAgendaEditor
+const submitAgendaForBill = gql`
+  mutation submitAgendaForBill(
+    $billNumber: Int!,
+    $corrlelation: Int!,
+    $description: String,
+    $agendaId: ID!,
+    $creatorId: ID!
+  ) {
+    createBillAgenda(
+      billNumber: $billNumber,
+      corrlelation: $corrlelation,
+      description: $description,
+      agendaId: $agendaId,
+      creatorId: $creatorId
+    ) {
+      id
+    }
+  }
+`;
+
+const scheme = gql`
+  mutation createAgenda(
+    $id: Int!,
+    $name: String!
+  ) {
+    createAgenda (
+     originalId: $id,
+     name: $name,
+   ) {
+     id
+   }
+  }
+`
+
+export default compose(
+  graphql(scheme),
+  withStyles(styles)
+)(BillAgendaEditor)
